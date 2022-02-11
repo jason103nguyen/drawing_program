e44193a2f04b8820a7153e091331adbb8139fe6b
Nguyen Tri Phuong <jason103nguyen@gmail.com>
2022-02-11 08:59:20 +0700

    add test case create canvas with cmd: C 20 2

--- src/test/java/DrawProgramTest.java
+++ src/test/java/DrawProgramTest.java
@@ -23,4 +23,23 @@ public class DrawProgramTest {
             Assert.assertArrayEquals(frameExpected[i], frameActual[i]);
         }
     }
+
+    @Test
+    public void testCreateANewCanvas_Cmd_C_20_2() {
+
+        String[][] frameExpected = new String[][] {
+                {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+                {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"}
+        };
+
+        DrawProgram drawProgram = new DrawProgram();
+        String[][] frameActual = drawProgram.draw("C 20 2");
+
+        Assert.assertEquals(frameExpected.length, frameActual.length);
+        for (int i = 0; i < frameExpected.length; i++) {
+            Assert.assertArrayEquals(frameExpected[i], frameActual[i]);
+        }
+    }
 }
