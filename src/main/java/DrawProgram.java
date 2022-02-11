39e11094ea533de8503842768b21aa72f4b8d630
Nguyen Tri Phuong <jason103nguyen@gmail.com>
2022-02-11 09:06:22 +0700

    implement code for test case create canvas with cmd: C 20 2

--- src/main/java/DrawProgram.java
+++ src/main/java/DrawProgram.java
@@ -2,14 +2,25 @@ public class DrawProgram {
 
     public String[][] draw(String cmd) {
 
-        String[][] frame = new String[][] {
-                {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
-                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
-                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
-                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
-                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
-                {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"}
-        };
+        String[] parameter = cmd.split(" ");
+        int width = Integer.valueOf(parameter[1]);
+        int height = Integer.valueOf(parameter[2]);
+
+        String[][] frame = new String[height + 2][width + 2];
+        int rowFrame = frame.length;
+        int colFrame = frame[0].length;
+
+        for (int row = 0; row < rowFrame ; row++) {
+            for (int col = 0; col < colFrame; col++) {
+                if (row == 0 || row == rowFrame - 1) {
+                    frame[row][col] = "-";
+                } else if (col == 0 || col == colFrame - 1) {
+                    frame[row][col] = "|";
+                } else {
+                    frame[row][col] = " ";
+                }
+            }
+        }
 
         return frame;
     }
