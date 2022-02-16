4509ed1fb99d8f39bb15cf137097092b5256ba83
Nguyen Tri Phuong <jason103nguyen@gmail.com>
2022-02-16 11:13:06 +0700

    refactor: Add comment for class Const

--- src/main/java/Const.java
+++ src/main/java/Const.java
@@ -1,12 +1,24 @@
 public class Const {
 
     public static final int OFFSET = 2;
+
+    /**
+     * CMD
+     */
     public static final String CMD_CREATE_FRAME = "C";
     public static final String CMD_DRAW_LINE = "L";
+
+    /**
+     * Limit of size
+     */
     public static final int MINIMUM_SIZE_HEIGHT = 1;
     public static final int MINIMUM_SIZE_WIDTH = 1;
     public static final int MAXIMUM_SIZE_WIDTH = 50;
     public static final int MAXIMUM_SIZE_HEIGHT = 50;
+
+    /**
+     * Color
+     */
     public static final String BORDER_HORIZONTAL = "-";
     public static final String BORDER_VERTICAL = "|";
     public static final String COLOR_LINE = "x";
