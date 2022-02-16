b1b69b1c82528b2be989e92bb5fb6bc57b9c9ec7
Nguyen Tri Phuong <jason103nguyen@gmail.com>
2022-02-16 15:33:37 +0700

    implement test case: draw rectangle - have width is 6

--- src/main/java/DrawProgram.java
+++ src/main/java/DrawProgram.java
@@ -112,16 +112,66 @@ public class DrawProgram {
         return frame;
     }
 
-    public String[][] drawRectangle(String[][] frame, String cmd) {
+    public String[][] drawLine(String[][] frame, int x1, int y1, int x2, int y2) throws CanvasException {
 
-        String[][] canvas = new String[][] {
-                {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
-                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "x", "x", "x", "x", "x", " ", " ", "|"},
-                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "x", " ", " ", " ", "x", " ", " ", "|"},
-                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "x", "x", "x", "x", "x", " ", " ", "|"},
-                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
-                {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"}
-        };
+        if (x1 <= 0 || y1 <= 0 || x2 <= 0 || y2 <= 0) {
+            throw new CanvasException("Coordinates cannot be less than or equal to 0");
+        }
+
+        int firstRow = 0;
+        int width = frame[firstRow].length - Const.OFFSET;
+        int height = frame.length - Const.OFFSET;
+
+        if (x1 > width || y1 > height || x2 > width || y2 > height) {
+            throw new CanvasException("Coordinates cannot pass the size of the frame");
+        }
+
+        if (x1 != x2 && y1 != y2) {
+            throw new CanvasException("Not a straight line");
+        }
+
+        if (y1 == y2) {
+            int row = y1;
+
+            for (int col = x1; col <= x2; col++) {
+                frame[row][col] = Const.COLOR_LINE;
+            }
+        } else if (x1 == x2){
+            int col = x1;
+
+            for (int row = y1; row <= y2; row++) {
+                frame[row][col] = Const.COLOR_LINE;
+            }
+        }
+
+        return frame;
+    }
+
+    public String[][] drawRectangle(String[][] frame, String cmd) throws CanvasException {
+
+        String[] arguments = cmd.split(Const.SPACE_CHARACTER);
+        int indexArgumentOfCmd = 0;
+        int indexAX_1 = 1;
+        int indexAY_1 = 2;
+        int indexBX_1 = 3;
+        int indexBY_1 = 4;
+
+        String cmdDrawLine = arguments[indexArgumentOfCmd];
+
+        int AX_1 = Integer.valueOf(arguments[indexAX_1]);
+        int AY_1 = Integer.valueOf(arguments[indexAY_1]);
+        int BX_1 = Integer.valueOf(arguments[indexBX_1]);
+        int BY_1 = Integer.valueOf(arguments[indexBY_1]);
+
+        int AX_2 = BX_1;
+        int AY_2 = AY_1;
+        int BX_2 = AX_1;
+        int BY_2 = BY_1;
+
+        String[][] canvas = drawLine(frame, AX_1, AY_1, AX_2, AY_2);
+        canvas = drawLine(canvas, BX_2, BY_2, BX_1, BY_1);
+        canvas = drawLine(canvas, AX_1, AY_1, BX_2, BY_2);
+        canvas = drawLine(canvas, AX_2, AY_2, BX_1, BY_1);
 
         return canvas;
     }
